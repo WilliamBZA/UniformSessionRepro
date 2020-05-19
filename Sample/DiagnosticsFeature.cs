9ba59228caea8b44b44a7af5866902452553a565
WilliamBZA <william.brander@gmail.com>
2020-05-19 11:06:29 +0200

    Repro

---
+++ Sample/DiagnosticsFeature.cs
@@ -0,0 +1,44 @@
+﻿using NServiceBus;
+using NServiceBus.Features;
+using NServiceBus.UniformSession;
+using System.Threading.Tasks;
+
+#region DiagnosticsFeature
+
+public class DiagnosticsFeature :
+    Feature
+{
+    internal DiagnosticsFeature()
+    {
+        EnableByDefault();
+
+        DependsOn("UniformSessionFeature");
+    }
+
+    protected override void Setup(FeatureConfigurationContext context)
+    {
+        context.Container.ConfigureComponent<CustomLogger>(DependencyLifecycle.InstancePerUnitOfWork);
+        context.Container.ConfigureComponent<DiagnosticsFeatureStartup>(DependencyLifecycle.InstancePerUnitOfWork);
+        context.RegisterStartupTask(b => b.Build<DiagnosticsFeatureStartup>());
+    }
+}
+
+public class DiagnosticsFeatureStartup : FeatureStartupTask
+{
+    public DiagnosticsFeatureStartup(IUniformSession session)
+    {
+
+    }
+
+    protected override Task OnStart(IMessageSession session)
+    {
+        return Task.CompletedTask;
+    }
+
+    protected override Task OnStop(IMessageSession session)
+    {
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
